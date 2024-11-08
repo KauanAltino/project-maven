4463821b9bf993520a5af48d994dbf6a604a2617
KauanAltino <kauanaltino@gmail.com>
2024-11-08 10:58:20 -0300

    Inicializando projeto maven

---
+++ helpdesk/src/main/java/com/maven_project/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.maven_project.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
---
+++ helpdesk/src/test/java/com/maven_project/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven_project.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
